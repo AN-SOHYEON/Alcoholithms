c08ae70b3bba0400a8e138f9b3c69b63f398e689
anso33 <hyeon131812@gmail.com>
2022-09-23 19:38:57 +0900

    study about string

--- 0920-구현,코너케이스/string.cpp
+++ 0920-구현,코너케이스/string.cpp
@@ -8,11 +8,22 @@ int	main () {
 	string a, b;
 	a = "4242";
 	b = "3333";
-	
-	b = a.back() + b;
+	string c = "01:12";
 
+	b = a.back() + b;
+	
+	
 	cout << "a : " << a << "\n";
+	int i = stoi(c);
 	cout << "b : " << b << "\n";
+	
+	c = c.substr(3, 2);
+
+	cout << "b : " << b << "\n";
+	int j = stoi(c);
+
+	cout << "i : " << i << "\n";
+	cout << "j : " << j << "\n";
 
 	return (0);
 }
