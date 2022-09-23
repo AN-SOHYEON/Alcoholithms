874eadac49e081f35dcf9421599881de429a679f
anso33 <hyeon131812@gmail.com>
2022-09-23 19:55:49 +0900

    not finish

---
+++ 0920-구현,코너케이스/1713.cpp
@@ -0,0 +1,22 @@
+/*
+0920-구현코너케이스 / 선택
+
+*/
+
+#include <iostream>
+
+using namespace	std;
+
+int main () {
+
+	int pic_num, stu_num;
+	cin >> pic_num >> stu_num;
+
+	
+	while (stu_num--) {
+		
+		// votePic();
+	}
+
+	return 0;
+}
