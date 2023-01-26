9d891ffc102356a1d718a26bc372d4c4d2b311a4
anso33 <hyeon131812@gmail.com>
2023-01-26 19:57:46 +0900

    feat: new algorithm study start in 42seoul

---
+++ algo_stud2/src/Solution_67260.java
@@ -0,0 +1,6 @@
+public class Solution_67260 {
+  public boolean solution(int n, int[][] path, int[][] order) {
+    boolean answer = true;
+    return answer;
+  }
+}
---
+++ algo_stud2/src/Solution_86971.java
@@ -0,0 +1,7 @@
+public class Solution_86971 {
+  public int solution(int n, int[][] wires) {
+    int answer = -1;
+    
+    return answer;
+  }
+}
