c19537547d91022fbcf980855d9c92d4482b2a74
anso33 <hyeon131812@gmail.com>
2023-03-06 16:43:21 +0900

    FEAT: 프로그래머스 최소직사각형

---
+++ algo_stud2/src/level1/Solution_86491.java
@@ -0,0 +1,27 @@
+package level1;
+
+class Solution {
+
+  public int solution(int[][] sizes) {
+    for (int i = 0; i < sizes.length; i++) {
+      if (sizes[i][0] < sizes[i][1]) {
+        int tmp = sizes[i][0];
+        sizes[i][0] = sizes[i][1];
+        sizes[i][1] = tmp;
+      }
+    }
+
+    int weight = sizes[0][0];
+    int height = sizes[0][1];
+    for (int i = 1; i < sizes.length; i++) {
+      if (weight < sizes[i][0]) {
+        weight = sizes[i][0];
+      }
+      if (height < sizes[i][1]) {
+        height = sizes[i][1];
+      }
+    }
+    System.out.println(weight * height);
+    return weight * height;
+  }
+}
